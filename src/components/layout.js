71a133da60081520be5dd8aded68c01bb312a827
System Administrator <root@T1MB97524.local>
2019-11-27 23:22:06 +0300

    added bootstrap import to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from "./header"
 import "./layout.css"
